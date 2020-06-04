7dd3855b69be6c9de59994b3817dbd3f34cd14ca
Saithis <saithis88@gmail.com>
2020-06-04 13:54:49 +0200

    test(select): toggle all tests

--- projects/prosoft-components-demo/src/app/select-demo/select-demo.module.ts
+++ projects/prosoft-components-demo/src/app/select-demo/select-demo.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCheckboxModule } from '@angular/material';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatRadioModule } from '@angular/material/radio';
 import { RouterModule } from '@angular/router';
