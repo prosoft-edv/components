5df2a654d70a6bca318e13ee2cc07a87f49b6ac6
Saithis <saithis@users.noreply.github.com>
2020-10-22 11:30:00 +0200

    chore: remove debug code

--- projects/prosoft-components-demo/src/app/block-ui-demo/block-ui-demo.component.ts
+++ projects/prosoft-components-demo/src/app/block-ui-demo/block-ui-demo.component.ts
@@ -1,15 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ViewEncapsulation,
-  QueryList,
-  ContentChildren,
-  OnInit,
-  AfterContentInit,
-  OnChanges,
-  Input,
-} from '@angular/core';
-import { MatOption } from '@angular/material/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-block-ui-demo',
@@ -54,10 +43,6 @@ import { MatOption } from '@angular/material/core';
       </mat-card>
     </ps-block-ui>
     <div style="height: 100vh"></div>
-
-    <app-ps-select *ngIf="blocked" [test]="true">
-      <div></div>
-    </app-ps-select>
   `,
   styles: [
     `
@@ -69,125 +54,7 @@ import { MatOption } from '@angular/material/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class BlockUiDemoComponent implements OnInit, AfterContentInit {
+export class BlockUiDemoComponent {
   public blocked = true;
   public spinnerText = 'some custom text that will be displayed while the view is blocked';
-  constructor() {
-    console.log('constructor x');
-  }
-
-  ngAfterContentInit(): void {
-    console.log('ngAfterContentInit x');
-  }
-  ngOnInit(): void {
-    console.log('ngOnInit x');
-  }
-}
-
-@Component({
-  selector: 'app-ps-select',
-  template: `
-    <app-mat-select [test]="test">
-      <app-test-data [test]="test"></app-test-data>
-    </app-mat-select>
-  `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
-})
-export class TestPsSelectComponent implements OnInit, AfterContentInit, OnChanges {
-  @Input() get test(): boolean {
-    return this._test;
-  }
-  set test(value: boolean) {
-    this._test = value;
-    console.log('ps-select input');
-  }
-  _test: boolean;
-
-  constructor() {
-    console.log('ps-select constructor');
-  }
-
-  ngOnChanges(): void {
-    console.log('ps-select ngOnChanges');
-  }
-  ngAfterContentInit(): void {
-    console.log('ps-select ngAfterContentInit');
-  }
-  ngOnInit(): void {
-    console.log('ps-select ngOnInit');
-  }
-}
-
-@Component({
-  selector: 'app-mat-select',
-  template: ` <ng-content></ng-content> `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
-})
-export class Test2Component implements OnInit, AfterContentInit, OnChanges {
-  @Input() get test(): boolean {
-    return this._test;
-  }
-  set test(value: boolean) {
-    this._test = value;
-    console.log('mat-select input');
-  }
-  _test: boolean;
-
-  @ContentChildren(MatOption, { descendants: true }) set options(value: QueryList<MatOption>) {
-    console.log('mat-select value', value);
-  }
-
-  constructor() {
-    console.log('mat-select constructor');
-  }
-
-  ngOnChanges(): void {
-    console.log('mat-select ngOnChanges');
-  }
-  ngAfterContentInit(): void {
-    console.log('mat-select ngAfterContentInit');
-  }
-  ngOnInit(): void {
-    console.log('mat-select ngOnInit');
-  }
-}
-
-@Component({
-  selector: 'app-test-data',
-  template: `
-    <ng-content></ng-content>
-    <mat-option>test</mat-option>
-  `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
-})
-export class TestComponent implements OnInit, AfterContentInit, OnChanges {
-  @Input() get test(): boolean {
-    return this._test;
-  }
-  set test(value: boolean) {
-    this._test = value;
-    console.log('test-data input');
-  }
-  _test: boolean;
-
-  @ContentChildren(MatOption) set options(value: QueryList<MatOption>) {
-    console.log('test-data value', value);
-  }
-
-  constructor(matSelect: Test2Component) {
-    console.log('test-data constructor', matSelect);
-  }
-
-  ngOnChanges(): void {
-    console.log('test-data ngOnChanges');
-  }
-  ngAfterContentInit(): void {
-    console.log('test-data ngAfterContentInit');
-  }
-  ngOnInit(): void {
-    console.log('test-data ngOnInit');
-  }
 }
--- projects/prosoft-components-demo/src/app/block-ui-demo/block-ui-demo.module.ts
+++ projects/prosoft-components-demo/src/app/block-ui-demo/block-ui-demo.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
 import { PsBlockUiModule } from '@prosoft/components/block-ui';
-import { BlockUiDemoComponent, Test2Component, TestComponent, TestPsSelectComponent } from './block-ui-demo.component';
+import { BlockUiDemoComponent } from './block-ui-demo.component';
 
 @NgModule({
   imports: [
@@ -25,9 +24,8 @@ import { BlockUiDemoComponent, Test2Component, TestComponent, TestPsSelectCompon
     MatInputModule,
     MatFormFieldModule,
     MatCheckboxModule,
-    MatOptionModule,
   ],
-  declarations: [BlockUiDemoComponent, TestComponent, Test2Component, TestPsSelectComponent],
+  declarations: [BlockUiDemoComponent],
   providers: [],
 })
 export class BlockUiDemoModule {}
