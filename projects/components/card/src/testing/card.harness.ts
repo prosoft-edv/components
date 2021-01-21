54faf695510ee609f763f2e68499ce33392fbc7e
Michael Kuhn <michael.kuhn@prosoft.net>
2021-01-21 16:35:18 +0100

    test(ps-header): added tests for ps-header

--- projects/components/card/src/testing/card.harness.ts
+++
@@ -1,9 +0,0 @@
-import { BaseHarnessFilters, ContentContainerComponentHarness } from '@angular/cdk/testing';
-
-export interface PsViewHarnessFilters extends BaseHarnessFilters {
-  errorText?: string | RegExp;
-}
-
-export class PsHeaderHarness extends ContentContainerComponentHarness {
-  static hostSelector = 'ps-header';
-}
